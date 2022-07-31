94b2e8ce69b087918733648fc66202a19c1ad9d1
Peter Uadiale <peteuadiale1@gmail.com>
2022-07-31 13:15:22 +0100

    update Maps.jsx

--- components/Maps.jsx
+++ components/Maps.jsx
@@ -8,13 +8,13 @@ import React, {
 import {
   GoogleMap,
   useJsApiLoader,
-  Marker,
-  InfoWindow,
   DirectionsRenderer,
   Autocomplete,
   Circle,
-  MarkerClusterer,
+  MarkerF,
+  InfoWindowF,
 } from "@react-google-maps/api";
+import Geocode from "react-geocode";
 import { useStateContext } from "../contexts/ContextProvider";
 import { FaTimes, FaLocationArrow } from "react-icons/fa";
 
@@ -25,29 +25,37 @@ import {
   lightMapStyles,
 } from "../mapStyles";
 import churchData from "../data/churches.json";
-import parkData from "../data/skateboard-parks.json";
 
 function Map() {
+  const [libraries] = useState(["places"]);
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY,
-    libraries: ["places"],
+    libraries,
   });
 
-  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
-  // const center = useMemo(() => ({ lat: 6.95, lng: 3.23333 }), []);
-  const center = useMemo(() => ({ lat: 45.421532, lng: -75.697189 }), []);
+  Geocode.setApiKey(process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY);
+  Geocode.setLanguage("en");
+  Geocode.setLocationType("ROOFTOP");
+  Geocode.enableDebug();
+
+  // const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  const mapRef = useRef();
+  const [destination, setDestination] = useState("");
+  const center = useMemo(() => ({ lat: 6.95, lng: 3.23333 }), []);
+  // const center = useMemo(() => ({ lat: 45.421532, lng: -75.697189 }), []);
 
   const onLoad = useCallback(function callback(map) {
-    setMap(map);
+    mapRef.current = map;
   }, []);
 
   const onUnmount = useCallback(function callback(map) {
-    setMap(null);
+    mapRef.current = null;
   }, []);
 
   const { searchBox, setSearchBox, currentColor } = useStateContext();
   const [selectedChurch, setSelectedChurch] = useState(null);
-  const [home, setHome] = useState(null);
+  const [home, setHome] = useState("");
+  const [address, setAddress] = useState("");
 
   const [directionsResponse, setDirectionsResponse] = useState(null);
   const [distance, setDistance] = useState("");
@@ -56,21 +64,17 @@ function Map() {
   const [loading, setLoading] = useState(false);
 
   const originRef = useRef();
-  const destinationRef = useRef();
 
-  async function calculateRoute() {
+  async function calculateRoute(church) {
     try {
       setError(false);
-      if (
-        originRef.current.value === "" ||
-        destinationRef.current.value === ""
-      ) {
+      if (originRef.current.value === "") {
         return;
       }
       const directionsService = new window.google.maps.DirectionsService();
       const results = await directionsService.route({
         origin: originRef.current.value,
-        destination: destinationRef.current.value,
+        destination: church,
         travelMode: window.google.maps.TravelMode.DRIVING,
       });
       await setDirectionsResponse(results);
@@ -81,7 +85,7 @@ function Map() {
       setError(error);
     } finally {
       setLoading(false);
-      setSearchBox(!searchBox);
+      // setSearchBox(!searchBox);
     }
   }
 
@@ -90,7 +94,6 @@ function Map() {
     setDistance("");
     setDuration("");
     originRef.current.value = "";
-    destinationRef.current.value = "";
   }
 
   useEffect(() => {
@@ -106,6 +109,29 @@ function Map() {
     };
   }, []);
 
+  const getGeoCode = useCallback(() => {
+    Geocode.fromAddress(originRef?.current?.value).then(
+      (response) => {
+        const { lat, lng } = response.results[0].geometry.location;
+        setHome({ lat, lng });
+        home && mapRef.current?.panTo(new window.google.maps.LatLng(lat, lng));
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }, []);
+
+  let myStyles = [
+    {
+      featureType: "poi",
+      elementType: "labels",
+      stylers: [{ visibility: "off" }],
+    },
+  ];
+
+  // if (home) console.log(home);
+
   return isLoaded ? (
     <div className="map relative">
       <GoogleMap
@@ -119,48 +145,65 @@ function Map() {
           zoomControl: false,
           mapTypeControl: false,
           fullscreenControl: false,
-          styles: mapStyles,
+          styles: [myStyles, mapStyles],
         }}
       >
-        {
-          <MarkerClusterer>
-            {(clusterer) => {
-              parkData.features.map((church) => (
-                <Marker
-                  key={church.properties.PARK_ID}
-                  position={{
-                    lat: church.geometry.coordinates[1],
-                    lng: church.geometry.coordinates[0],
-                  }}
-                  clusterer={clusterer}
-                  onClick={() => {
-                    setSelectedChurch(church);
-                  }}
-                  icon={{
-                    url: `../../church.svg`,
-                    scaledSize: new window.google.maps.Size(25, 25),
-                  }}
-                />
-              ));
-            }}
-          </MarkerClusterer>
-        }
+        {home && (
+          <>
+            <MarkerF
+              position={home}
+              icon={{
+                url: "../../home.svg",
+                scaledSize: new window.google.maps.Size(25, 25),
+              }}
+            />
+
+            {churchData.features.map((church) => (
+              <MarkerF
+                key={church.properties.CHURCH_ID}
+                position={{
+                  lat: church.geometry.coordinates[0],
+                  lng: church.geometry.coordinates[1],
+                }}
+                onClick={() => {
+                  const address = church.properties.ADDRESS;
+                  const name = church.properties.NAME;
+                  setAddress(name);
+                  setSelectedChurch(church);
+                  calculateRoute(address);
+                }}
+                icon={{
+                  url: `../../church.svg`,
+                  scaledSize: new window.google.maps.Size(25, 25),
+                }}
+              />
+            ))}
+
+            <Circle center={home} radius={15000} options={home} />
+            <Circle center={home} radius={30000} options={middleOptions} />
+            <Circle center={home} radius={45000} options={farOptions} />
+          </>
+        )}
+
         {selectedChurch && (
-          <InfoWindow
+          <InfoWindowF
             onCloseClick={() => {
               setSelectedChurch(null);
             }}
             position={{
-              lat: selectedChurch.geometry.coordinates[1],
-              lng: selectedChurch.geometry.coordinates[0],
+              lat: selectedChurch.geometry.coordinates[0],
+              lng: selectedChurch.geometry.coordinates[1],
             }}
           >
-            <div>
-              <h2>{selectedChurch.properties.NAME}</h2>
-              <p>{selectedChurch.properties.DESCRIPTIO}</p>
+            <div className="p-2">
+              <h2 className="p-2 font-bold text-md">
+                {selectedChurch.properties.NAME}
+              </h2>
+              <p>{selectedChurch.properties.DESCRIPTION}</p>
             </div>
-          </InfoWindow>
+          </InfoWindowF>
         )}
+
         {directionsResponse && (
           <DirectionsRenderer
             directions={directionsResponse}
@@ -179,7 +222,7 @@ function Map() {
         <div className="absolute right-0 md:right-4 mx-2 md:m-0 top-28 md:top-4 bg-light-gray dark:bg-main-dark-bg p-4 rounded-lg">
           <div className="flex gap-2 flex-wrap">
             <div>
-              <Autocomplete>
+              <Autocomplete onPlaceChanged={() => getGeoCode()}>
                 <input
                   type="text"
                   placeholder="Home"
@@ -188,59 +231,47 @@ function Map() {
                 />
               </Autocomplete>
             </div>
-            <div>
-              <Autocomplete>
-                <input
-                  type="text"
-                  placeholder="Loveworld branch"
-                  ref={destinationRef}
-                  className="outline-none border dark:bg-light-gray rounded-md p-2 text-slate-800"
-                />
-              </Autocomplete>
-            </div>
 
             <div className="flex items-center gap-2 mb-4">
-              <button
-                type="submit"
-                onClick={calculateRoute}
-                style={{ backgroundColor: currentColor }}
-                className="p-2 text-sm text-white rounded-lg"
-              >
-                Calculate Route
-              </button>
               <span
                 onClick={clearRoute}
                 className="text-red-600 dark:text-red-400 border p-1 text-xs rounded-md cursor-pointer"
               >
                 Clear
               </span>
+              <span
+                style={{ backgroundColor: currentColor }}
+                className="rounded-lg flex justify-center items-center w-10 h-10 cursor-pointer"
+                onClick={() => {
+                  mapRef.current?.panTo(center);
+                  mapRef.current?.setZoom(12);
+                  setSearchBox(false);
+                }}
+              >
+                <FaLocationArrow className="text-white" />
+              </span>
+              <span>{loading && <div>Loading...</div>}</span>
             </div>
           </div>
           <div className="flex flex-wrap justify-between">
             {distance && duration ? (
-              <>
+              <div className="flex flex-wrap gap-2 flex-col">
                 <p className="text-sm text-slate-800 dark:text-gray-200">
-                  Distance: {distance}{" "}
+                  The distance between{" "}
+                  <strong className="text-[16px]">
+                    {originRef?.current?.value}
+                  </strong>{" "}
+                  and{" "}
+                  <strong className="text-[16px]">{address && address}</strong>{" "}
+                  is: <strong className="text-[16px]">{distance}</strong>{" "}
                 </p>
                 <p className="text-sm text-slate-800 dark:text-gray-200">
                   Duration: {`${duration} By Car`}{" "}
                 </p>
-              </>
+              </div>
             ) : (
               ""
             )}
-            <span
-              style={{ backgroundColor: currentColor }}
-              className="rounded-lg flex justify-center items-center w-10 h-10 cursor-pointer"
-              onClick={() => {
-                map.panTo(center);
-                map.setZoom(12);
-                setSearchBox(false);
-              }}
-            >
-              <FaLocationArrow className="text-white" />
-            </span>
-            <span>{loading && <div>Loading...</div>}</span>
           </div>
           {error && (
             <p className="text-red-500 text-xs mt-2">
@@ -257,3 +288,36 @@ function Map() {
 }
 
 export default React.memo(Map);
+
+const defaultOptions = {
+  strokeOpacity: 0.5,
+  strokeWeight: 2,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+};
+
+const closeOptions = {
+  ...defaultOptions,
+  zIndex: 3,
+  fillOpacity: 0.05,
+  strokeColor: "#8BC34A",
+  fillColor: "#8BC34A",
+};
+
+const middleOptions = {
+  ...defaultOptions,
+  zIndex: 2,
+  fillOpacity: 0.05,
+  strokeColor: "#FBC02D",
+  fillColor: "#FBC02D",
+};
+
+const farOptions = {
+  ...defaultOptions,
+  zIndex: 1,
+  fillOpacity: 0.05,
+  strokeColor: "#FF5252",
+  fillColor: "#FF5252",
+};
